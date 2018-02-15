5f3603273c53f26275902f1e8c33090f42329501
Giresse Ashu-Bob <ashubobg@yahoo.co.uk>
2018-02-16 00:46:49 +0200

    adding working files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAXF780eBaw89euQewtLuAPOpzNm8tuUug';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component to produce some html
+// props are passed by this.props in the class components
+class App extends Component{
+	constructor(props) {
+		super(props);
+	  this.state = {
+	  	videos: [],
+        selectedVideo:null
+	  	};
+	  	this.videoSearch('surfboards');
+	}
+    videoSearch(term) {
+      YTSearch({key: API_KEY, term: term}, (videos) => {
+	    this.setState({
+	    	videos: videos,
+	    	selectedVideo:videos[0]
+	    });	    
+      });
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    }
+  render() {
+  	//debounce returns a function that can only be called after 300ms
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
+   return (
+	  <div>
+	  <SearchBar onSearchTermChange={videoSearch} />
+	  <VideoDetail video={this.state.selectedVideo} />
+	  <VideoList
+       onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+	   videos={this.state.videos} />
+	  </div> 
+    );
+  }
+}
+
+// display to the DOM
+ReactDom.render(<App />, document.querySelector('.container'));
